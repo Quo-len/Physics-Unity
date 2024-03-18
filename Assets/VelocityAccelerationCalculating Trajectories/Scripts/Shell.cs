1da276908dadc3dbfbb8103b0da96e4d1d263e63
Quo-len <kotenko.vadym1121@vu.cdu.edu.ua>
2024-03-18 05:20:45 +0200

    first commit

---
+++ Assets/VelocityAccelerationCalculating Trajectories/Scripts/Shell.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public GameObject explosion;
+    float speed = 0f;
+    float yspeed = 0f;
+    float mass = 10f;
+    float force = 3f;
+    float drag = 1f;
+    float gravity = -9.8f;
+    float gAccel;
+    float acceleration;
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tank" || col.gameObject.tag == "ground")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        acceleration = force / mass;
+        speed += 1 * acceleration;
+        gAccel = gravity / mass;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        speed *= (1 - Time.deltaTime * drag);
+        yspeed += gAccel * Time.deltaTime;
+        this.transform.Translate(0, yspeed, speed);
+    }
+}
